fb509db8f468996aba3f12e37f3d69acc11ccd4e
John C. Kennedy <devjckennedy@gmail.com>
2020-07-03 13:02:32 +0900

    Configure axios

--- store-front/src/main.js
+++ store-front/src/main.js
@@ -1,16 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import vueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
-import './main.css'
+import { plainAxiosInstance, securedAxiosInstance } from './backend/axios'
+import './main.css' // tailwind
 
 Vue.config.productionTip = false
+Vue.use(vueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   components: { App },
   template: '<App/>'
 })
